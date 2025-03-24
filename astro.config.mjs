efc352b99239feb0dfaae48d8ae3eb6b54301ae0
Evlakhovych Ihor Vitaliyovych <127222921+1Mk7i@users.noreply.github.com>
2025-03-24 17:21:26 +0200

    3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
-import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://feature-tour.netlify.app',
   integrations: [
-    preact(),
     react({
-        include: '**/*.jsx',
+      include: '**/*.jsx',
     }),
     sitemap({
-      canonicalURL: 'https://feature-tour.netlify.app'
-    })
+      canonicalURL: 'https://feature-tour.netlify.app',
+    }),
   ],
 });
